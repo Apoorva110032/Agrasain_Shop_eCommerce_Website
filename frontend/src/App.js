48bcbb2883b688329087fcf2ae6e0cb58e2a12a7
Apoorva <apoorva27sep@gmail.com>
2021-04-13 13:05:41 +0530

    Setup React for Project

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() { 
+  // or we can type 'const App = () => {}'
+  return (
+    <div className="App">
+        <h1>Welcome to Agrasain Shop</h1>
+    </div>
+  );
+}
+
+export default App;
